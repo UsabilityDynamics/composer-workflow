1a69f54cb3e02e6357dcf3845fc21ad2f64a948a
andypotanin <andy.potanin@usabilitydynamics.com>
2014-11-12 18:17:51 -0500

    [ci skip]

--- readme.md
+++ readme.md
@@ -1,10 +1,18 @@
-
 ### Install Global Plugin
 ```
-composer global show UsabilityDynamics/composer-workflow
 composer global require UsabilityDynamics/composer-workflow
 ```
 
-### Notes
-The plugin will be installed into ~/.composer/vendor/UsabilityDynamics/composer-workflow
+### Get Information and Plugin
+```
+composer global show UsabilityDynamics/composer-workflow
+```
 
+### Notes
+* The plugin will be installed into ~/.composer/vendor/UsabilityDynamics/composer-workflow
+* This plugin will be loaded when using Composer within any other project once installed globally.
+* We could use the "archive" command to build release shit.
+
+### ToDO
+
+* Set UDR for defaultRepositories (https://github.com/composer/composer/blob/master/src/Composer/Config.php#L49).
--- src/UsabilityDynamics/Composer/Workflow/Plugin.php
+++ src/UsabilityDynamics/Composer/Workflow/Plugin.php
@@ -9,40 +9,115 @@
  * file that was distributed with this source code.
  */
 
-namespace UsabilityDynamics\Composer\Workflow;
+namespace UsabilityDynamics\Composer\Workflow {
 
-use Composer\Composer;
-use Composer\EventDispatcher\EventSubscriberInterface;
-use Composer\IO\IOInterface;
-use Composer\Plugin\PluginInterface;
-use Composer\Plugin\PluginEvents;
-use Composer\Plugin\PreFileDownloadEvent;
+	use Composer\Composer;
+	use Composer\EventDispatcher\EventSubscriberInterface;
+	use Composer\Config;
+	use Composer\IO\IOInterface;
+	use Composer\Plugin\PluginInterface;
+	use Composer\Plugin\PluginEvents;
+	use Composer\Plugin\PreFileDownloadEvent;
+	use Composer\Plugin\CommandEvent;
 
-/**
- * Composer Plugin for Workflow functionality
- *
- * @author Nils Adermann <naderman@naderman.de>
- */
-class Plugin implements PluginInterface, EventSubscriberInterface {
+	//$ application = new Application();
 
-    protected $composer;
-    protected $io;
+	// s$application->run();
+	// die( '-sadflkjf');
 
-    public function activate(Composer $composer, IOInterface $io) {
-	    // die( "all your base are belong to us.'\n");
-        $this->composer = $composer;
-        $this->io = $io;
-    }
+	//die( '<pre>' . print_r( $application->getIO(), true ) . '</pre>');
 
-    public static function getSubscribedEvents() {
+	/**
+	 * Composer Plugin for Workflow functionality
+	 *
+	 * @author Nils Adermann <naderman@naderman.de>
+	 */
+	class Plugin implements PluginInterface, EventSubscriberInterface {
 
-        return array(
-            // PluginEvents::PRE_FILE_DOWNLOAD => array( array('onPreFileDownload', 0) ),
-        );
+		protected $composer;
+		protected $io;
 
-    }
+		/**
+		 * @see https://getcomposer.org/doc/articles/plugins.md
+		 *
+		 * @param Composer $composer
+		 * @param IOInterface $io
+		 */
+		public function activate( Composer $composer, IOInterface $io ) {
+			//die( "all your base are belong to us. \n");
 
-    public function onPreFileDownload(PreFileDownloadEvent $event) {
+			// Creates new config instance...
+			// $this->config = new Config();
 
-    }
+			// Gets actual instance config.
+			// die( '<pre>' . print_r( $composer->getConfig(), true ) . '</pre>');
+			// die( '<pre>' . print_r( $composer->getPluginManager(), true ) . '</pre>');
+
+			// get a config setting
+			// die( $this->config->get( 'vendor-dir' ) );
+			// die( Composer::BRANCH_ALIAS_VERSION );
+
+			 # die( '<pre>' . print_r( get_class_methods( $composer ), true ) . '</pre>');
+
+			if( method_exists( $composer, 'setPackage' ) ){
+				// die('setPackage');
+			}
+			//$composer->setDefaultCommand();
+
+			$this->composer = $composer;
+			$this->io       = $io;
+
+			//$io->ask( 'Wanna party?' );
+			// $io->askAndValidate( 'Really wanna party?' );
+			//$io->write( 'asdf' );
+
+			// die( '<pre>' . print_r( $composer->getConfig(), true ) . '</pre>');
+			// $installer = new TemplateInstaller($io, $composer);
+			// $composer->getInstallationManager()->addInstaller($installer);
+
+			// Workflow\PushCommand();
+
+		}
+
+		public static function getSubscribedEvents() {
+
+			return array(
+				PluginEvents::PRE_FILE_DOWNLOAD => array( array( 'onPreFileDownload', 0 ) ),
+				PluginEvents::COMMAND           => array( array( 'commandHandler', 0 ) ),
+			);
+
+		}
+
+		/**
+		 * Catches All Evensts, inclduing script events.
+		 *
+		 * @see https://getcomposer.org/doc/articles/scripts.md#event-names
+		 *
+		 * @param CommandEvent $event
+		 */
+		public function commandHandler( CommandEvent $event ) {
+
+			//$_interface = new CLI();
+			//$_interface->write( 'asdf' );
+			// IOInterface::ask( 'asdf' );
+
+			//$this->io->write( $event->getName() . ' -> ' . '' , true );
+			//$this->io->write( $event->getCommandName() . ' -> ' . '' , true );
+			// $this->io->write( $event->getName() . ' -> ' . $event->getCommandName(), true );
+
+		}
+
+		public function onPreFileDownload( PreFileDownloadEvent $event ) {
+
+			$protocol = parse_url( $event->getProcessedUrl(), PHP_URL_SCHEME );
+
+			if ( $protocol === 'gs' ) {
+				// $awsClient = new GoogleStorageClient($this->io, $this->composer->getConfig());
+				// $s3RemoteFilesystem = new S3RemoteFilesystem($this->io, $event->getRemoteFilesystem()->getOptions(), $awsClient);
+				// $event->setRemoteFilesystem($s3RemoteFilesystem);
+			}
+		}
+
+	}
+
 }
