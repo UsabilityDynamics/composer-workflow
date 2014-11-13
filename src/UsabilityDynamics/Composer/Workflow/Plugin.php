23e0ac29a85bc8b83a3781e45080bd8dd9a748d9
andypotanin <andy.potanin@usabilitydynamics.com>
2014-11-12 19:14:14 -0500

    [ci skip]

--- src/UsabilityDynamics/Composer/Workflow/Plugin.php
+++ src/UsabilityDynamics/Composer/Workflow/Plugin.php
@@ -8,7 +8,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace UsabilityDynamics\Composer\Workflow {
 
 	use Composer\Composer;
@@ -30,7 +29,6 @@ namespace UsabilityDynamics\Composer\Workflow {
 	/**
 	 * Composer Plugin for Workflow functionality
 	 *
-	 * @author Nils Adermann <naderman@naderman.de>
 	 */
 	class Plugin implements PluginInterface, EventSubscriberInterface {
 
